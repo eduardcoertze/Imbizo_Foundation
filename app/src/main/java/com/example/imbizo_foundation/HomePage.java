81d1ab0fe63aed45fcdcbbe9201839d944262a11
Eddie Coertze <johndoe@example.comeddiecoertze@gmail.com>
2022-06-27 19:59:56 +0200

    initial-10-Nav-Bar

--- app/src/main/java/com/example/imbizo_foundation/HomePage.java
+++ app/src/main/java/com/example/imbizo_foundation/HomePage.java
@@ -1,19 +1,29 @@
 package com.example.imbizo_foundation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity{
 
     ArrayList<CoursesModel> coursesModels = new ArrayList<>();
+
+    ImageView btnNav;
 
     int[] coursesImages = {R.drawable.va_programming, R.drawable.va_stocks, R.drawable.va_photography, R.drawable.va_teaching,
             R.drawable.va_fitness, R.drawable.va_music, R.drawable.va_art };
@@ -23,8 +33,13 @@ public class HomePage extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        btnNav = findViewById(R.id.btnNav);
+
+        NavigationView navView = findViewById(R.id.navigation_view);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
         RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
-        Button btnQuiz = findViewById(R.id.btn_quiz);
 
         setUpCoursesModels();
         C_RecyclerViewAdapter adapter = new C_RecyclerViewAdapter(this, coursesModels);
@@ -32,12 +47,36 @@ public class HomePage extends AppCompatActivity{
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        btnQuiz.setOnClickListener(new View.OnClickListener() {
+        btnNav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(HomePage.this, QuizActivity.class);
-                startActivity(i);
-                finish();
+                // Opens the side menu
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                // Gets selected item
+                int id = item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id) {
+
+
+                    case R.id.nav_test:
+                        //Go to Tutorial activity
+                        Intent quizIntent = new Intent(HomePage.this, QuizActivity.class);
+                        startActivity(quizIntent);
+                        finish();
+                        break;
+                    default:
+                        return true;
+
+                }
+
+                return true;
             }
         });
 
--- app/src/main/java/com/example/imbizo_foundation/Result.java
+++ app/src/main/java/com/example/imbizo_foundation/Result.java
@@ -50,40 +50,4 @@ public class Result {
     }
 
 
-
-
-//    public static ArrayList<Result> getResults(){
-//
-//        ArrayList<Result> resultsList = new ArrayList<Result>();
-//
-//        Result result1 = new Result(
-//                1,
-//                "Introvert",
-//                "a typically reserved or quiet person who tends to be introspective and enjoys spending time alone"
-//        );
-//        resultsList.add(result1);
-//
-//        Result result2 = new Result(
-//                2,
-//                "Extrovert",
-//                "A typically gregarious and unreserved person who enjoys and seeks out social interaction"
-//        );
-//        resultsList.add(result2);
-//
-//        Result result3 = new Result(
-//                3,
-//                "Ambivert",
-//                "A person who has a balance of extrovert and introvert features in their personality."
-//        );
-//        resultsList.add(result3);
-//
-//        Result result4 = new Result(
-//                3,
-//                "Omnivert",
-//                "Someone who is an introvert and extrovert depending on situations"
-//        );
-//        resultsList.add(result4);
-//
-//        return resultsList;
-//    }
 }
